9d06528f0842bf6f0016731141998a754c23c494
YID <yi19871230@hotmail.com>
2021-06-21 11:42:59 +0800

    New add fild

--- type.py
+++ type.py
@@ -1,13 +1,21 @@
 import random
+Scoring = 0
+count = -1
+correct = 0
 engirish = ['Engirish', 'apple', 'kid', 'for', ]
 while True:
 	r = random.choice(engirish)
-
 	while True:
+		count += 1
 		print(r)
 		type = input('請輸入英文單字:')
-		if type == ('q'):
+		if type == (r):
+			correct += 1
 			break
-		elif type == (r):
-			break
-	break
+		elif type == ('q'):
+			break	
+		else :
+			Scoring += 1	
+	if type == ('q'):
+		break
+print('總共答錯了', Scoring, '題', '正確率', correct / count * 100, '%')	
