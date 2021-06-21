e1ab7789fbc23707ba8fd12795969fddcc38d6e5
YID <yi19871230@hotmail.com>
2021-06-22 03:37:58 +0800

    Add file

--- type.py
+++ type.py
@@ -2,11 +2,20 @@ import random
 Scoring = 0
 count = -1
 correct = 0
-engirish = ['Engirish', 'apple', 'kid', 'for', ]
+d = []
+engirish = []#'Engirish', 'apple', 'kid', 'for'
+
+with open('word.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		name, price, = line.strip().split(',')
+		engirish.append([name, price,])
 while True:
-	r = random.choice(engirish)
+	t = random.choice(engirish)
 	while True:
+		r = t[0]
+		c = t[1]
 		count += 1
+		print(c)
 		print(r)
 		type = input('請輸入英文單字:')
 		if type == (r):
@@ -18,4 +27,6 @@ while True:
 			Scoring += 1	
 	if type == ('q'):
 		break
-print('總共答錯了', Scoring, '題', '正確率', correct / count * 100, '%')	
+print('總共答題', count)
+print('總共答錯了', Scoring, '題')
+print('正確率', correct / count * 100, '%')
